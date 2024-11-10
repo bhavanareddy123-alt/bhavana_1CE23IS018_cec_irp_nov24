2a3fe2a317cffa58b61b5ea9e376ecb1b8179ee3
harshaprasadr <harshaprasad358@gmail.com>
2024-11-10 15:28:51 +0530

    day 5 folder

---
+++ day5/5h.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int n, revered = 0,remainder,original;
+    printf("Enter an integer:");
+    scanf("")
+    return 0;
+}
---
+++ day5/day6/6a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(){
+    char arr[100];
+    scanf("%[^\n]s",arr);
+    for(int i=0;arr[i]!='\0';i++)
+    {
+        if(!((arr[i]>='a'&&arr[i]<='z')||(arr[i]>='A'&&arr[i]<='Z')))
+        {
+            int j=1;
+            while(!((arr[i]>='Z')||(arr[i+j]>='A'&&arr[i+j]<='Z')))j++;
+            strcpy(&arr[i],&arr[i+j]);
+        }
+    }
+    printf("%s",arr);
+    return 0;
+}
